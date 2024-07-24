6007b567d1c8276df6c5f99d8537dc509e831d0a
ilirtz <ilt0055@my.londonmet.ac.uk>
2024-07-24 18:26:49 +0100

    Update GadgetShop.java

--- GadgetShop.java
+++ GadgetShop.java
@@ -2,7 +2,7 @@ import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.ArrayList;
-import java.awt.*;
+
 
 public class GadgetShop extends JFrame implements ActionListener {
     private ArrayList<Gadget> gadgets; // Arraylist to store gadgets
